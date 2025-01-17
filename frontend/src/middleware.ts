62d29572fd7751aece848a394f29db6c9d324e20
gwenael9 <gwenaelgueho@gmail.com>
2025-01-17 15:07:19 +0100

    middleware ok

---
+++ frontend/src/middleware.ts
@@ -0,0 +1,32 @@
+import { NextRequest, NextResponse } from "next/server";
+// import { jwtVerify } from "jose";
+
+// interface Payload {
+//   email: string;
+//   role: string;
+// }
+
+// const JWT_SECRET = Uint8Array.from(atob(process.env.JWT_SECRET || ""), (c) =>
+//   c.charCodeAt(0)
+// );
+
+export default async function middelware(request: NextRequest) {
+  const token = request.cookies.get("token")?.value;
+
+  if (!token) {
+    return NextResponse.redirect(new URL("/auth", request.url));
+  }
+
+  try {
+    // const { payload } = await jwtVerify<Payload>(token, JWT_SECRET);
+
+    // si j'essaie d'aller sur la page de connexion alors que je suis connecté
+    if (request.nextUrl.pathname.startsWith("/auth")) {
+      // je suis redirigé vers la page d'accueil
+      return NextResponse.redirect(new URL("/", request.url));
+    }
+    return NextResponse.next();
+  } catch (error) {
+    return NextResponse.next();
+  }
+}
